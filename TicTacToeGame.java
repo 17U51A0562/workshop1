4ff76d4ef5fc77384db61faccfcdf6cca9c2e7d6
17U51A0562 <vikasmudhiraj283@gmail.com>
2021-10-04 08:49:28 +0530

    the 1st use case is TicTacToe

---
+++ TicTacToeGame.java
@@ -0,0 +1,15 @@
+package logical.program;
+
+public class TicTacToeGame {
+	public static void main(String[] args) {
+		System.out.println("welcome to tic tac toe game:");
+		createBoard();
+	}
+	public static void createBoard() {
+	char createboard [] = new char[10];
+	for(int i = 1; i<createboard.length; i++) {
+		createboard[i] = ' ';
+	}
+	}
+}
+
